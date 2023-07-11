664887bbda32cfe48441d08ca361cdfe0a2a83e4
Jeff Barragan <leshz@me.com>
2023-07-10 22:44:58 -0500

    deploy: gcp app engine

--- src/plugins/mercado-pago/server/constants/constants.ts
+++ src/plugins/mercado-pago/server/constants/constants.ts
@@ -4,3 +4,10 @@ export const INVOICES_STATUS = {
   IN_PROCESS: "in_process",
   FAILED: "failed",
 };
+
+export const INVOICES_STATUS2 = {
+  PENDING: "pending",
+  APPROVED: "approved",
+  IN_PROCESS: "in_process",
+  FAILED: "failed",
+};
