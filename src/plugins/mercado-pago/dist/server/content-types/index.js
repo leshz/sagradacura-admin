3d6be4aefd12fefa5cc47d6f15bb68796fd2a98d
Jeff Barragan <leshz@me.com>
2023-07-11 23:20:27 -0500

    fix : deploy too gcp

--- src/plugins/mercado-pago/dist/server/content-types/index.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-var __importDefault = (this && this.__importDefault) || function (mod) {
-    return (mod && mod.__esModule) ? mod : { "default": mod };
-};
-Object.defineProperty(exports, "__esModule", { value: true });
-const invoice_1 = __importDefault(require("./invoice"));
-exports.default = { invoice: invoice_1.default };
