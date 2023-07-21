4425880367e0412331f545728a0bb272ae908e63
Jeff Barragan <leshz@me.com>
2023-07-20 21:40:12 -0500

    feat: deploy using domains

--- config/plugins.ts
+++ config/plugins.ts
@@ -1,17 +1,20 @@
-export default ({ env }) => ({
-  "mercado-pago": {
-    enabled: true,
-    resolve: "./src/plugins/mercado-pago",
-  },
-  upload: {
-    config: {
-      provider: "@strapi-community/strapi-provider-upload-google-cloud-storage",
-      providerOptions: {
-        bucketName: env("BUCKET_NAME"),
-        publicFiles: env("BUCKET_PUBLIC_FILES") === "true",
-        uniform: env("BUCKET_UNIFORM") === "true",
-        basePath: "",
+export default ({ env }) => {
+  return {
+    "mercado-pago": {
+      enabled: true,
+      resolve: "./src/plugins/mercado-pago",
+    },
+    upload: {
+      config: {
+        provider:
+          "@strapi-community/strapi-provider-upload-google-cloud-storage",
+        providerOptions: {
+          bucketName: env("BUCKET_NAME", "strapi"),
+          publicFiles: env.bool("BUCKET_PUBLIC_FILES", true),
+          uniform: env.bool("BUCKET_UNIFORM", true),
+          basePath: "",
+        },
       },
     },
-  },
-});
+  };
+};
