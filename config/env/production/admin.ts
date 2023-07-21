ee0654b08cb44d935c9df709f930b959aa77a159
Jeff Barragan <leshz@me.com>
2023-07-21 00:53:37 -0500

    refactor:  fix production config files

--- config/env/production/admin.ts
+++ config/env/production/admin.ts
@@ -1,6 +1,13 @@
+const favicon =
+  "https://storage.googleapis.com/shzcms.appspot.com/Impugny3_a6fd54cfca_0c6b18fbe4/Impugny3_a6fd54cfca_0c6b18fbe4.png";
+
 export default ({ env }) => ({
+  BROWSER: false,
+  url: "/adminn",
   auth: {
     secret: env("ADMIN_JWT_SECRET"),
+    logo: favicon,
+    menu: favicon,
   },
   apiToken: {
     salt: env("API_TOKEN_SALT"),
@@ -9,5 +16,8 @@ export default ({ env }) => ({
     token: {
       salt: env("TRANSFER_TOKEN_SALT"),
     },
+  },
+  head: {
+    favicon: favicon,
   },
 });
