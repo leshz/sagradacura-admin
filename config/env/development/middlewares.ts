95c2adc043dad983acf6e5d92287096d8c5b7b17
Jeff Barragan <leshz@me.com>
2023-11-15 18:05:45 -0500

    s3 config

--- config/env/development/middlewares.ts
+++ config/env/development/middlewares.ts
@@ -1,6 +1,31 @@
 export default [
   "strapi::errors",
-  "strapi::security",
+  {
+    name: "strapi::security",
+    config: {
+      contentSecurityPolicy: {
+        useDefaults: true,
+        directives: {
+          "connect-src": ["'self'", "https:"],
+          "img-src": [
+            "'self'",
+            "data:",
+            "blob:",
+            "dl.airtable.com",
+            "strapi-aws-develop-bucket.s3.us-east-2.amazonaws.com",
+          ],
+          "media-src": [
+            "'self'",
+            "data:",
+            "blob:",
+            "dl.airtable.com",
+            "strapi-aws-develop-bucket.s3.us-east-2.amazonaws.com",
+          ],
+          upgradeInsecureRequests: null,
+        },
+      },
+    },
+  },
   "strapi::cors",
   "strapi::poweredBy",
   "strapi::logger",
--- config/env/development/plugins.ts
+++ config/env/development/plugins.ts
@@ -4,5 +4,25 @@ export default ({ env }) => {
       enabled: true,
       resolve: "./src/plugins/payments",
     },
+    upload: {
+      config: {
+        provider: "aws-s3",
+        providerOptions: {
+          accessKeyId: env("AWS_ACCESS_KEY_ID"),
+          secretAccessKey: env("AWS_ACCESS_SECRET"),
+          region: env("AWS_REGION"),
+          params: {
+            ACL: env("AWS_ACL", "public-read"),
+            signedUrlExpires: env("AWS_SIGNED_URL_EXPIRES", 15 * 60),
+            Bucket: env("AWS_BUCKET"),
+          },
+        },
+        actionOptions: {
+          upload: {},
+          uploadStream: {},
+          delete: {},
+        },
+      },
+    },
   };
 };
