eb2b13ab13e43d322c36d346c18abe40b4af28d8
Jeff Barragan <leshz@me.com>
2023-11-17 11:19:40 -0500

    s3 reconfig

--- config/env/production/plugins.ts
+++ config/env/production/plugins.ts
@@ -7,7 +7,7 @@ export default ({ env }) => {
     upload: {
       config: {
         provider: "aws-s3",
-        providerOptions: {
+        s3Options: {
           accessKeyId: env("AWS_ACCESS_KEY_ID"),
           secretAccessKey: env("AWS_ACCESS_SECRET"),
           region: env("AWS_REGION"),
