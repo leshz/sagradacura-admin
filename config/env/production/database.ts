b50874bc95c47ffd972ea63492c629acb6bd613d
Jeff Barragan <leshz@me.com>
2024-05-21 13:08:03 -0500

    fix debug

--- config/env/production/database.ts
+++ config/env/production/database.ts
@@ -50,7 +50,6 @@ export default ({ env }) => {
         },
         schema: env("DATABASE_SCHEMA", "public"),
       },
-      debug: true,
       pool: {
         min: env.int("DATABASE_POOL_MIN", 0),
         max: env.int("DATABASE_POOL_MAX", 10),
