1098c0e32009d3baec492ba5e3137c93e7264c43
Jeff Barragan <leshz@me.com>
2024-06-14 20:06:27 -0500

    blog get by slug

--- src/api/blog/controllers/blog.ts
+++ src/api/blog/controllers/blog.ts
@@ -2,6 +2,20 @@
  * blog controller
  */
 
-import { factories } from '@strapi/strapi'
+import { factories } from "@strapi/strapi";
 
-export default factories.createCoreController('api::blog.blog');
+export default factories.createCoreController(
+  "api::blog.blog",
+  ({ strapi }) => ({
+    async findOne(ctx) {
+      await this.validateQuery(ctx);
+      const { id } = ctx.params;
+      const entity = await strapi.db.query("api::blog.blog").findOne({
+        where: { slug: id },
+        populate: true,
+      });
+      const sanitizedEntity = await this.sanitizeOutput(entity, ctx);
+      return this.transformResponse(sanitizedEntity);
+    },
+  })
+);
