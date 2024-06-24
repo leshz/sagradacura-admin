ab241003bedd1159ada06382c8213cd72862dd6c
Jeffer Barragán <leshz@me.com>
2024-06-24 10:35:57 -0500

    Fix Invoice admin view issue 🐛  (#11)
    * Update strapi version
    
    * Update schema product
    
    * Fix invoice admin view

--- src/plugins/strapi-ecommerce-mercadopago/server/bootstrap.ts
+++ src/plugins/strapi-ecommerce-mercadopago/server/bootstrap.ts
@@ -1,6 +1,6 @@
 import { Strapi } from "@strapi/strapi";
-import shipping from "./components/shipping.json";
-import shopper from "./components/shopper.json";
+import shipping from "../components/mercadopago/shipping.json";
+import shopper from "../components/mercadopago/shopper.json";
 
 export default async ({ strapi }: { strapi: Strapi }) => {
   const shippingComponent = strapi.components["mercadopago.shipping"];
@@ -8,7 +8,24 @@ export default async ({ strapi }: { strapi: Strapi }) => {
 
   if (!shopperComponent && !shippingComponent) {
     strapi.log.info("Register new components");
-    strapi.components["mercadopago.shipping"] = shipping as any;
-    strapi.components["mercadopago.shopper"] = shopper as any;
+    const res = await strapi
+      .plugin("content-type-builder")
+      .services.components.createComponent({
+        component: {
+          category: "mercadopago",
+          displayName: shipping.info.displayName,
+          icon: shipping.info.icon,
+          attributes: shipping.attributes,
+        },
+        components: [
+          {
+            tmpUID: "mercadopago.shopper",
+            category: "mercadopago",
+            displayName: shopper.info.displayName,
+            icon: shopper.info.icon,
+            attributes: shopper.attributes,
+          },
+        ],
+      });
   }
 };
