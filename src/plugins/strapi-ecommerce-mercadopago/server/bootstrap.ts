76021d226556756c82b5687f18310933182cf74c
Jeff Barragan <leshz@me.com>
2024-06-23 23:12:45 -0500

    Fix plugin updater schema

--- src/plugins/strapi-ecommerce-mercadopago/server/bootstrap.ts
+++ src/plugins/strapi-ecommerce-mercadopago/server/bootstrap.ts
@@ -1,5 +1,14 @@
-import { Strapi } from '@strapi/strapi';
+import { Strapi } from "@strapi/strapi";
+import shipping from "./components/shipping.json";
+import shopper from "./components/shopper.json";
 
-export default ({ strapi }: { strapi: Strapi }) => {
-  // bootstrap phase
+export default async ({ strapi }: { strapi: Strapi }) => {
+  const shippingComponent = strapi.components["mercadopago.shipping"];
+  const shopperComponent = strapi.components["mercadopago.shopper"];
+
+  if (!shopperComponent && !shippingComponent) {
+    strapi.log.info("Register new components");
+    strapi.components["mercadopago.shipping"] = shipping as any;
+    strapi.components["mercadopago.shopper"] = shopper as any;
+  }
 };
